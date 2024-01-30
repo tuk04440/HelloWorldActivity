adbd01a27fa9e3b2178c67b4f8e79cb93083fd11
Allen <tuk04440@temple.edu>
2024-01-30 12:10:33 -0500

    Initialized views.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-    lateinit var displayEditText: EditText
-    lateinit var button: Button
+    lateinit var nameEditText: EditText
+    lateinit var clickMeButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
 
         
         // Respond to button click event per specifications
